832d6e3afefb08054248b4e58559513a93095592
NEXUSCODE-DEV94 <nam636743@gmail.com>
2026-01-10 22:05:19 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,8 @@
 import os
-import discord
-from datetime import timedelta
 
 TOKEN = os.environ.get("DISCORD_TOKEN")
 
 ADMIN_ROLE_ID = [1313086280141373441, 1452291945413083247]
-
 TICKET_CATEGORY_ID = 1450086411956129894
 YUZU_TICKET_CATEGORY_ID = 1455540840708702300
 DONE_CATEGORY_ID = 1456845967545471157
@@ -20,13 +17,6 @@ GUILD_ID = 1313077923741438004
 CHANNEL_ID = 1457317342488035502
 UPDATE_INTERVAL = 300
 
-ITEMS = {
-    1: {"name": "[🍿] Netflix, Amazon Prime 無料", "price": 0, "stock": 9999999999, "url": "https://net20.cc/login2"},
-    2: {"name": "[📩]Gmail 無限", "price": 0, "stock": 9999999999, "url": "https://smailpro.com"},
-    3: {"name": "[🚮]捨てアド", "price": 0, "stock": 9999999999, "url": "https://www.gmailnator.com\nhttps://m.kuku.lu\nhttps://sute.jp\nhttps://dropmail.me"},
-    4: {"name": "[📱]TikTok 無料・ウォーターマークなしダウンロード", "price": 0, "stock": 9999999999, "url": "https://tiktokio.com/ja/"},
-}
-
 NUKE_GIFS = [
     "https://i.pinimg.com/originals/3a/e7/92/3ae792706e97941696b70b4763bd2963.gif",
     "https://i.pinimg.com/originals/08/b4/f3/08b4f35b31e0ea0948ca7b5778e32b54.gif",
@@ -35,5 +25,9 @@ NUKE_GIFS = [
     "https://i.pinimg.com/originals/6a/8e/4d/6a8e4d2b450f10d3733422efc4e95526.gif",
 ]
 
-PATTERN_NORMAL = r"^(.+?)・(.+)$"
-PATTERN_QUOTED = r"^『(.+?)』｜(.+)$"
+ITEMS = {
+    1: {"name":"[🍿] Netflix, Amazon Prime 無料","price":0,"stock":9999999999,"url":"https://net20.cc/login2"},
+    2: {"name":"[📩]Gmail 無限","price":0,"stock":9999999999,"url":"https://smailpro.com"},
+    3: {"name":"[🚮]捨てアド","price":0,"stock":9999999999,"url":"https://www.gmailnator.com\nhttps://m.kuku.lu\nhttps://sute.jp\nhttps://dropmail.me"},
+    4: {"name":"[📱]TikTok 無料・ウォーターマークなしダウンロード","price":0,"stock":9999999999,"url":"https://tiktokio.com/ja/"},
+}
